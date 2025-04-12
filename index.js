442f47af225acf3e73bc09224a5c248e1905c84b
Dirk Kok <the@dirkkok.nl>
2025-04-13 00:14:16 +0200

    read images comma separated

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    const images = core.getInput('images');
-    console.log(`Hello ${nameToGreet}!`);
+    const images = core.getInput('images').split(",");
     console.log(`${images.length} images. The first is ${images[0]}`);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
